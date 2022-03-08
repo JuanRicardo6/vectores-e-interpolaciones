909e5eec7afe699a567c02a465ab3b154a197c86
Ricardo <henry3197@hotmail.com>
2022-03-08 09:20:47 -0500

    Fuerzas

---
+++ SistemasInteractivos/Assets/Fuerzas/FuerzaGravitacional.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuerzaGravitacional : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 acceleration;
+    [SerializeField]
+    private Vector3 velocity;
+
+    [SerializeField] Vector3 r;
+    [SerializeField] bool quieto;
+
+    [SerializeField] GameObject masaAjena;
+    [SerializeField] float masaA;
+    [SerializeField] bool limitarVelocidad;
+    [SerializeField] float limiteVelocidad;
+
+    public float mass;
+
+
+    float areaFrontal;
+
+
+
+    void Awake()
+    {
+        masaA = masaAjena.GetComponent<FuerzaGravitacional>().mass;
+
+    }
+    void Update()
+    {
+        Move(quieto);
+
+    }
+    public void Move(bool quieto)
+    {
+        if (quieto == true)
+        {
+            return;
+        }
+        r = masaAjena.transform.position - this.transform.position;
+        acceleration = Vector3.zero;
+
+        ApplyForce(FuerzaG(masaA, r));
+
+        velocity += acceleration * Time.deltaTime;
+        velocity = LimitarVelocidad(limitarVelocidad, velocity, limiteVelocidad);
+        transform.position += velocity * Time.deltaTime;
+    }
+    private void ApplyForce(Vector3 force)
+    {
+        acceleration += force / mass;
+    }
+    private Vector3 FuerzaG(float masaA, Vector3 r)
+    {
+        Vector3 fg = new Vector3();
+        fg = r.normalized * (mass * masaA) / (r.magnitude * r.magnitude);
+        return fg;
+    }
+    private Vector3 LimitarVelocidad(bool limitar, Vector3 velocidad, float limite)
+    {
+
+        if (limitar == true && velocidad.magnitude > limite)
+        {
+            velocidad = velocidad.normalized * limite;
+            return velocidad;
+        }
+        else
+        {
+            return velocidad;
+        }
+    }
+}
---
+++ SistemasInteractivos/Assets/Fuerzas/PhysicMove.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PhysicMove : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 acceleration;
+    private Vector3 velocity;
+
+    [SerializeField] bool tocandoAgua, tocandoAire;
+    [SerializeField]
+    [Range(0, 1)]
+    private float dampFactor, coeficienteDeFriccion, coAg, coAi;
+    [SerializeField] private float mass, gravedad;
+    [SerializeField] private Vector3 fuerza;
+
+    float areaFrontal;
+    private void Start()
+    {
+        areaFrontal = transform.localScale.x;
+    }
+    private void Update()
+    {
+
+        Move();
+        CheckLimits();
+
+    }
+
+    public void Move()
+    {
+        acceleration = Vector3.zero;
+        //ApplyForce(fuerza);
+        //ApplyForce(Friccion(fuerza));
+        ApplyForce(new Vector3(0, gravedad * mass, 0));
+        ApplyForce(ResistenciaFluido(new Vector3(0, gravedad * mass, 0), tocandoAgua));
+        velocity += acceleration * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+
+
+    }
+
+    private void CheckLimits()
+    {
+        Vector3 position = transform.position;
+        if ((position.x > 4.5f || position.x < -4.5f))
+        {
+            if (position.x > 4.5f) transform.position = new Vector3(4.5f, transform.position.y);
+            if (position.x < -4.5f) transform.position = new Vector3(-4.5f, transform.position.y);
+
+            velocity.x = velocity.x * -1;
+            velocity *= dampFactor;
+        }
+
+        else if (position.y > 4.5f || position.y < -4.5f)
+        {
+
+            if (position.y > 4.5f) transform.position = new Vector3(transform.position.x, 4.5f);
+            if (position.y < -4.5f) transform.position = new Vector3(transform.position.x, -4.5f);
+            velocity.y = velocity.y * -1;
+            velocity *= dampFactor;
+        }
+    }
+
+    private void ApplyForce(Vector3 force)
+    {
+        acceleration += force / mass;
+    }
+    private Vector3 Friccion(Vector3 force)
+    {
+        return -velocity.normalized * coeficienteDeFriccion * force.magnitude;
+    }
+    private Vector3 ResistenciaFluido(Vector3 force, bool tocandoAgua)
+    {
+        if (tocandoAgua == true)
+        {
+            Debug.Log("entro");
+            return -velocity.normalized * coAg * velocity.magnitude * velocity.magnitude * 0.5f * areaFrontal;
+        }
+        else if (tocandoAire == true)
+        {
+            return -velocity.normalized * coAi * velocity.magnitude * velocity.magnitude * 0.5f * areaFrontal;//-velocity.normalized * coeficienteDeFriccion * force.magnitude;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Agua"))
+        {
+            tocandoAgua = true;
+        }
+        if (other.CompareTag("Aire"))
+        {
+            tocandoAire = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Agua"))
+        {
+            tocandoAgua = false;
+        }
+
+    }
+
+    //private void CheckLimits()
+    //{
+    //    // 
+    //    var pos = transform.position;
+
+    //    if (Mathf.Abs(pos.x) > limits.x)
+    //    {
+    //        pos = (Vector3)new Vector2(limits.x * Mathf.Sign(pos.x), pos.y);
+    //        transform.position = pos;
+    //        velocity = (Vector3)new Vector2(Mathf.Abs(velocity.x)*-Mathf.Sign(velocity.x),velocity.y)*dampFactor;
+    //    }
+    //    if (Mathf.Abs(pos.y) > limits.y)
+    //    {
+    //        pos = (Vector3)new Vector2(pos.x, limits.y*Mathf.Sign(pos.y));
+    //        transform.position = pos;
+    //        velocity = (Vector3)new Vector2(velocity.x, Mathf.Abs(velocity.y) * -Mathf.Sign(velocity.y))*dampFactor;
+    //    }
+    //}
+}
